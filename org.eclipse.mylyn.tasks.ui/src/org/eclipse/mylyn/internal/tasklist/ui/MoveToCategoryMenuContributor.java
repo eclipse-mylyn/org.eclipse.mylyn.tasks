526cdf1b3d31118513a4118e25b86359f6158a9b
cvs2git <>
2006-06-29 18:18:22 +0000

    This commit was manufactured by cvs2svn to create branch 'e_3_1'.
    Cherrypick from master 2006-06-29 18:18:21 UTC mkersten 'Progress on: ui nits for 0.6':
        org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/TrustAllSslProtocolSocketFactory.java
        org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/BugzillaSearchDialogTest.java
        org.eclipse.mylyn.help.ui/doc/images/faq/changeset-subclipse.gif
        org.eclipse.mylyn.help.ui/doc/images/faq/changesets-model-based.gif
        org.eclipse.mylyn.tasks.tests/plugin.xml
        org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasklist/tests/AllTaskListTests.java
        org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasklist/tests/RepositoryTaskSynchronizationTest.java
        org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasklist/tests/TaskAttachmentActionsTest.java
        org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasklist/tests/TaskListContentProviderTest.java
        org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasklist/tests/TaskRepositoriesExternalizerTest.java
        org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasklist/tests/mockconnector/MockAttributeFactory.java
        org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasklist/tests/mockconnector/MockRepositoryExternalizer.java
        org.eclipse.mylyn.tasks.tests/testdata/taskdataimporttest/repositories.xml
        org.eclipse.mylyn.tasks.ui/icons/etool16/comment.gif
        org.eclipse.mylyn.tasks.ui/icons/etool16/person.gif
        org.eclipse.mylyn.tasks.ui/icons/eview16/notification-close.gif
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/RetrieveTitleFromUrlJob.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/MoveToCategoryMenuContributor.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/TaskListNotificationIncoming.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/TaskListNotificationQueryIncoming.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/actions/CopyToClipboardAction.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/actions/SaveRemoteFileAction.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/editors/TaskEditorActionContributor.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/wizards/InputAttachmentSourcePage.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/wizards/NewAttachmentPage.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/wizards/NewAttachmentWizard.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/wizards/NewAttachmentWizardDialog.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/wizards/NewLocalTaskWizard.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/util/SaxRepositoriesContentHandler.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/util/SaxRepositoriesWriter.java
        org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/util/TaskRepositoriesExternalizer.java

---
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/MoveToCategoryMenuContributor.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 Mylar committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.mylar.internal.tasklist.ui;
+
+import java.util.List;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.mylar.provisional.tasklist.AbstractQueryHit;
+import org.eclipse.mylar.provisional.tasklist.AbstractTaskContainer;
+import org.eclipse.mylar.provisional.tasklist.ITask;
+import org.eclipse.mylar.provisional.tasklist.ITaskListElement;
+import org.eclipse.mylar.provisional.tasklist.MylarTaskListPlugin;
+
+/**
+ * @author Mik Kersten
+ */
+public class MoveToCategoryMenuContributor implements IDynamicSubMenuContributor {
+
+	private static final String LABEL = "Move to Category";
+
+	public MenuManager getSubMenuManager(final List<ITaskListElement> selectedElements) {
+		final MenuManager subMenuManager = new MenuManager(LABEL);
+
+		for (final AbstractTaskContainer category : MylarTaskListPlugin.getTaskListManager().getTaskList()
+				.getCategories()) {
+			if (!category.equals(MylarTaskListPlugin.getTaskListManager().getTaskList().getArchiveContainer())) {
+				Action action = new Action() {
+					@Override
+					public void run() {
+						for (ITaskListElement element : selectedElements) {
+							if (element instanceof ITask) {
+								MylarTaskListPlugin.getTaskListManager().getTaskList().moveToContainer(category,
+										(ITask) element);
+							} else if (element instanceof AbstractQueryHit) {
+								ITask task = ((AbstractQueryHit) element).getCorrespondingTask();
+								if (task != null) {
+									MylarTaskListPlugin.getTaskListManager().getTaskList().moveToContainer(category,
+											task);
+								}
+							}
+						}
+					}
+				};
+				action.setText(category.getDescription());
+				action.setImageDescriptor(TaskListImages.CATEGORY);
+				if (selectedElements.size() == 1 && selectedElements.get(0) instanceof AbstractQueryHit) {
+					AbstractQueryHit hit = (AbstractQueryHit) selectedElements.get(0);
+					if (hit.getCorrespondingTask() == null) {
+						action.setEnabled(false);
+					}
+				}
+				subMenuManager.add(action);
+			}
+		}
+		return subMenuManager;
+	}
+
+}

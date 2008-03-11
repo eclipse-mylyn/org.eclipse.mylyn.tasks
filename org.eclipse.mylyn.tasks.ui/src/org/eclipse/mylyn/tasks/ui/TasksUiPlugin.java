6c3b9bcd14bdc68af3d5aa269f4bb623cb543d31
spingel <>
2008-03-11 21:56:13 +0000

    RESOLVED - bug 219690: [m2.3.1] NPE when a remote task is changed but the property is not mapped https://bugs.eclipse.org/bugs/show_bug.cgi?id=219690

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/TasksUiPlugin.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/TasksUiPlugin.java
@@ -1078,6 +1078,9 @@ public class TasksUiPlugin extends AbstractUIPlugin {
 	}
 
 	private static String cleanValue(String value) {
+		if (value == null) {
+			return "";
+		}
 		String commentText = value.replaceAll("\\s", " ").trim();
 		if (commentText.length() > 60) {
 			commentText = commentText.substring(0, 55) + "...";

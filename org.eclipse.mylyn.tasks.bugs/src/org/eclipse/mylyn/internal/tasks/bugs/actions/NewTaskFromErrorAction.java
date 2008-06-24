353b47e2dcf885f3f2ca3ebaae9d7461a0db62b5
cvs2git <>
2008-06-24 08:10:40 +0000

    This commit was manufactured by cvs2svn to create tag 'R_3_0_0_e_3_3'.
    Sprout from master 2008-06-24 08:10:38 UTC spingel 'NEW - bug 238039: port Trac test cases to Mylyn 3.0'
    Cherrypick from master 2007-05-29 12:53:13 UTC mkersten 'RESOLVED - bug 189557: update about.html license files to Europa date':
        org.eclipse.mylyn-feature/about.html
        org.eclipse.mylyn.bugzilla-feature/about.html
    Cherrypick from e_3_3_m_3_x 2008-06-20 18:03:47 UTC spingel 'Version update':
        org.eclipse.mylyn-feature/feature.xml
        org.eclipse.mylyn.bugzilla-feature/feature.xml
        org.eclipse.mylyn.sdk-feature/feature.xml
        org.eclipse.mylyn.tasks.bugs/.classpath
        org.eclipse.mylyn.tasks.bugs/META-INF/MANIFEST.MF
        org.eclipse.mylyn.tasks.bugs/about.html
        org.eclipse.mylyn.tasks.bugs/plugin.xml
        org.eclipse.mylyn.tasks.bugs/schema/pluginRepositoryMappings.exsd
        org.eclipse.mylyn.tasks.bugs/schema/taskContributors.exsd
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/provisional/tasks/bugs/AbstractTaskContributor.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/AttributeTaskMapper.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/DefaultTaskContributor.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/IRepositoryConstants.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/TaskContributorManager.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/TaskErrorReporter.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/TasksBugsPlugin.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/actions/NewTaskFromErrorAction.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/actions/ReportBugAction.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/BundleGroupContainer.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/ErrorLogStatus.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/FeatureGroup.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/FeatureStatus.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/ReportBugOrEnhancementWizard.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/ReportErrorPage.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/ReportErrorWizard.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/SelectFeaturePage.java
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/wizards/SelectProductPage.java
    Delete:
        org.eclipse.mylyn.tasks.bugs/icons/etool16/task-new.gif
        org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/actions/NewTaskFromMarkerHandler.java

--- org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/actions/NewTaskFromErrorAction.java
+++ org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/actions/NewTaskFromErrorAction.java
@@ -13,13 +13,17 @@ package org.eclipse.mylyn.internal.tasks.bugs.actions;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.mylyn.internal.commons.core.ErrorReporterManager;
 import org.eclipse.mylyn.internal.tasks.bugs.wizards.ErrorLogStatus;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.internal.views.log.LogEntry;
-import org.eclipse.ui.internal.views.log.LogSession;
+import org.eclipse.pde.internal.runtime.logview.LogEntry;
+import org.eclipse.pde.internal.runtime.logview.LogSession;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
 
 /**
  * Creates a new task from the selected error log entry.
@@ -27,11 +31,11 @@ import org.eclipse.ui.internal.views.log.LogSession;
  * @author Jeff Pound
  * @author Steffen Pingel
  */
-public class NewTaskFromErrorAction implements IObjectActionDelegate {
+public class NewTaskFromErrorAction implements IViewActionDelegate, ISelectionChangedListener {
 
 	public static final String ID = "org.eclipse.mylyn.tasklist.ui.repositories.actions.create";
 
-	private LogEntry entry;
+	private TreeViewer treeViewer;
 
 	/**
 	 * Fills a {@link StringBuilder} with {@link LogEntry} information, optionally including subentries too
@@ -78,7 +82,7 @@ public class NewTaskFromErrorAction implements IObjectActionDelegate {
 //			includeChildren = true;
 //		}
 //		StringBuilder sb = new StringBuilder();
-//		buildDescriptionFromLogEntry(entry, sb, true);
+//		buildDescriptionFromLogEntry(entry, sb, includeChildren);
 
 		ErrorLogStatus status = new ErrorLogStatus(entry.getSeverity(), entry.getPluginId(), entry.getCode(),
 				entry.getMessage());
@@ -92,6 +96,15 @@ public class NewTaskFromErrorAction implements IObjectActionDelegate {
 	}
 
 	public void run() {
+		TreeItem[] items = treeViewer.getTree().getSelection();
+		LogEntry entry = null;
+		if (items.length > 0) {
+			entry = (LogEntry) items[0].getData();
+		}
+		if (entry == null) {
+			return;
+		}
+
 		createTask(entry);
 	}
 
@@ -99,14 +112,19 @@ public class NewTaskFromErrorAction implements IObjectActionDelegate {
 		run();
 	}
 
-	public void selectionChanged(IAction action, ISelection selection) {
-		Object object = ((IStructuredSelection) selection).getFirstElement();
-		if (object instanceof LogEntry) {
-			entry = (LogEntry) object;
-		}
+	public void init(IViewPart view) {
+		ISelectionProvider sp = view.getViewSite().getSelectionProvider();
+		sp.addSelectionChangedListener(this);
+		sp.setSelection(sp.getSelection());
 	}
 
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	public void selectionChanged(IAction action, ISelection selection) {
+		// this selection is always empty? explicitly register a listener in
+		// init() instead
+	}
+
+	public void selectionChanged(SelectionChangedEvent event) {
+		treeViewer = (TreeViewer) event.getSource();
 	}
 
 }

9a5d50d88e25ba0e11f2690ba8e608eca7dfd7bd
spingel <>
2009-06-02 03:54:05 +0000

    NEW - bug 248778: [releng] backport changes to Eclipse 3.3 https://bugs.eclipse.org/bugs/show_bug.cgi?id=248778

--- org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/DefaultSupportHandler.java
+++ org.eclipse.mylyn.tasks.bugs/src/org/eclipse/mylyn/internal/tasks/bugs/DefaultSupportHandler.java
@@ -14,14 +14,15 @@ package org.eclipse.mylyn.internal.tasks.bugs;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.runtime.IBundleGroup;
 import org.eclipse.core.runtime.IProduct;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.mylyn.internal.provisional.tasks.bugs.AbstractTaskContributor;
+import org.eclipse.mylyn.internal.provisional.tasks.bugs.AbstractSupportHandler;
+import org.eclipse.mylyn.internal.provisional.tasks.bugs.ISupportRequest;
 import org.eclipse.mylyn.internal.provisional.tasks.bugs.ISupportResponse;
 import org.eclipse.mylyn.internal.provisional.tasks.bugs.ITaskContribution;
 import org.eclipse.mylyn.internal.tasks.bugs.wizards.ErrorLogStatus;
@@ -38,11 +39,11 @@ import org.osgi.framework.Bundle;
 /**
  * @author Steffen Pingel
  */
-@SuppressWarnings("deprecation")
-public class DefaultTaskContributor extends AbstractTaskContributor {
+public class DefaultSupportHandler extends AbstractSupportHandler {
 
 	@Override
-	public void process(ITaskContribution contribution) {
+	public void preProcess(ISupportRequest request) {
+		ITaskContribution contribution = request.getDefaultContribution();
 		String description = getDescription(contribution.getStatus());
 		if (description != null) {
 			contribution.appendToDescription(description);
@@ -50,7 +51,17 @@ public class DefaultTaskContributor extends AbstractTaskContributor {
 	}
 
 	@Override
-	public void postProcess(ISupportResponse response) {
+	public void process(ITaskContribution contribution, IProgressMonitor monitor) {
+		if (contribution.getAttribute(IRepositoryConstants.DESCRIPTION) == null) {
+			String description = getDescription(contribution.getStatus());
+			if (description != null) {
+				contribution.appendToDescription(description);
+			}
+		}
+	}
+
+	@Override
+	public void postProcess(ISupportResponse response, IProgressMonitor monitor) {
 		IStatus contribution = response.getStatus();
 		TaskData taskData = response.getTaskData();
 		if (contribution instanceof ProductStatus) {
@@ -95,34 +106,42 @@ public class DefaultTaskContributor extends AbstractTaskContributor {
 	}
 
 	public void appendErrorDetails(StringBuilder sb, IStatus status, Date date) {
-		sb.append("\n\n");
+		sb.append("\n\n"); //$NON-NLS-1$
 		sb.append(Messages.DefaultTaskContributor_Error_Details);
 		if (date != null) {
 			sb.append("\n"); //$NON-NLS-1$
-			sb.append(NLS.bind("Date: {0}", date));
+			sb.append(NLS.bind(Messages.DefaultSupportHandler_Date_X, date));
 		}
 		sb.append("\n"); //$NON-NLS-1$
-		sb.append(NLS.bind("Message: {0}", status.getMessage()));
+		sb.append(NLS.bind(Messages.DefaultSupportHandler_Message_X, status.getMessage()));
 		sb.append("\n"); //$NON-NLS-1$
-		sb.append(NLS.bind("Severity: {0}", getSeverityText(status.getSeverity())));
+		sb.append(NLS.bind(Messages.DefaultSupportHandler_Severity_X, getSeverityText(status.getSeverity())));
+		appendProductInformation(sb);
+		sb.append("\n"); //$NON-NLS-1$
+		sb.append(NLS.bind(Messages.DefaultSupportHandler_Plugin_X, status.getPlugin()));
+	}
+
+	private void appendProductInformation(StringBuilder sb) {
 		IProduct product = Platform.getProduct();
 		if (product != null) {
 			sb.append("\n"); //$NON-NLS-1$
 			if (product.getName() != null) {
-				sb.append(NLS.bind("Product: {0} ({1})", product.getName(), product.getId()));
+				sb.append(NLS.bind(Messages.DefaultSupportHandler_Product_X, product.getName()));
 			} else {
-				sb.append(NLS.bind("Product: {0}", product.getId()));
+				sb.append(NLS.bind(Messages.DefaultSupportHandler_Product_X, product.getId()));
+			}
+			Bundle definingBundle = product.getDefiningBundle();
+			if (definingBundle != null) {
+				Object version = definingBundle.getHeaders().get("Bundle-Version"); //$NON-NLS-1$
+				if (version != null) {
+					sb.append(" "); //$NON-NLS-1$
+					sb.append(version);
+				}
+			}
+			if (product.getName() != null) {
+				sb.append(NLS.bind(" ({0})", product.getId())); //$NON-NLS-1$
 			}
 		}
-		sb.append("\n"); //$NON-NLS-1$
-		sb.append(NLS.bind("Plugin: {0}", status.getPlugin()));
-	}
-
-	@Override
-	public Map<String, String> getAttributes(IStatus status) {
-		Map<String, String> attributes = new HashMap<String, String>();
-		attributes.put(IRepositoryConstants.DESCRIPTION, getDescription(status));
-		return attributes;
 	}
 
 	public String getDescription(IStatus status) {
@@ -131,33 +150,44 @@ public class DefaultTaskContributor extends AbstractTaskContributor {
 			if (product.getBundleGroup() != null) {
 				StringBuilder sb = new StringBuilder();
 				sb.append("\n\n\n"); //$NON-NLS-1$
-				sb.append(Messages.DefaultTaskContributor_INSTALLED_FEATURES_AND_PLUGINS);
+				sb.append(Messages.DefaultSupportHandler_Configuration_Details);
+				appendProductInformation(sb);
+				sb.append("\n"); //$NON-NLS-1$
+				sb.append(Messages.DefaultSupportHandler_Installed_Features);
+				sb.append("\n"); //$NON-NLS-1$
 				for (IBundleGroup bundleGroup : new IBundleGroup[] { product.getBundleGroup() }) {
+					sb.append(" "); //$NON-NLS-1$
 					sb.append(bundleGroup.getIdentifier());
 					sb.append(" "); //$NON-NLS-1$
 					sb.append(bundleGroup.getVersion());
 					sb.append("\n"); //$NON-NLS-1$
 
-					Bundle[] bundles = bundleGroup.getBundles();
-					if (bundles != null) {
-						for (Bundle bundle : bundles) {
-							sb.append("  "); //$NON-NLS-1$
-							sb.append(bundle.getSymbolicName());
-							String version = (String) bundle.getHeaders().get(
-									Messages.DefaultTaskContributor_Bundle_Version);
-							if (version != null) {
-								sb.append(" "); //$NON-NLS-1$
-								sb.append(version);
-							}
-							sb.append("\n"); //$NON-NLS-1$
-						}
-					}
+//					Bundle[] bundles = bundleGroup.getBundles();
+//					if (bundles != null) {
+//						for (Bundle bundle : bundles) {
+//							sb.append("  "); //$NON-NLS-1$
+//							sb.append(bundle.getSymbolicName());
+//							String version = (String) bundle.getHeaders().get(
+//									Messages.DefaultTaskContributor_Bundle_Version);
+//							if (version != null) {
+//								sb.append(" "); //$NON-NLS-1$
+//								sb.append(version);
+//							}
+//							sb.append("\n"); //$NON-NLS-1$
+//						}
+//					}
 				}
 				return sb.toString();
 			}
 		} else if (status instanceof ErrorLogStatus) {
 			ErrorLogStatus errorLogStatus = (ErrorLogStatus) status;
 			StringBuilder sb = new StringBuilder();
+			sb.append("\n\n"); //$NON-NLS-1$
+			sb.append(Messages.DefaultSupportHandler_What_steps_message);
+			sb.append("\n"); //$NON-NLS-1$
+			sb.append(Messages.DefaultSupportHandler_Step_1);
+			sb.append(Messages.DefaultSupportHandler_Step_2);
+			sb.append(Messages.DefaultSupportHandler_Step_3);
 			appendErrorDetails(sb, errorLogStatus, errorLogStatus.getDate());
 			if (errorLogStatus.getLogSessionData() != null) {
 				sb.append(Messages.DefaultTaskContributor_SESSION_DATA);
